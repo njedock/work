a6556c07735abffb011a0dee5ed2ead2ea6daeb7
*parfait* <njedockparfait@gmail.com>
2025-11-08 02:25:21 -0700

    First commit - initial project setup

---
+++ exper.dart
@@ -0,0 +1,15 @@
+void main(){
+    
+String firstName= "john";
+print( "String:$firstName");
+
+
+int myNum=41;
+print( "int:$myNum");
+
+double myNum1=19.44;
+print("double:$myNum1");
+
+bool myBool= false;
+print("bool:$myBool");
+}
